1479e9373f79359b9bce2b91d49553a518e4ab2f
bennyblair <ben.m.blair@gmail.com>
2025-08-08 14:02:49 +1000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/emet-capital-elegance/', // must match repo name exactly
+  base: '/emet-capital-elegance/', // note the leading & trailing slash
 })
