5783b7ed4315b71ab2ee38e3b4d191a6f87e4eb8
bennyblair <ben.m.blair@gmail.com>
2025-08-08 15:56:48 +1000

    Create theme-provider.tsx

---
+++ src/components/theme-provider.tsx
@@ -0,0 +1,9 @@
+import * as React from "react"
+import type { ThemeProviderProps } from "next-themes"
+import { ThemeProvider as NextThemesProvider } from "next-themes"
+
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+}
+
+export default ThemeProvider
