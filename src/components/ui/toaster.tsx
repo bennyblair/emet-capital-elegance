42def23eac926d6cf235caaf0c31cdefadd71eff
bennyblair <ben.m.blair@gmail.com>
2025-08-08 16:04:03 +1000

    Update toaster.tsx

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -3,31 +3,28 @@ import {
   Toast,
   ToastClose,
   ToastDescription,
-  ToastProvider,
   ToastTitle,
   ToastViewport,
 } from "@/components/ui/toast"
 
 export function Toaster() {
-  const { toasts } = useToast()
+  const { toasts, dismiss } = useToast()
 
   return (
-    <ToastProvider>
-      {toasts.map(function ({ id, title, description, action, ...props }) {
-        return (
-          <Toast key={id} {...props}>
-            <div className="grid gap-1">
-              {title && <ToastTitle>{title}</ToastTitle>}
-              {description && (
-                <ToastDescription>{description}</ToastDescription>
-              )}
-            </div>
-            {action}
-            <ToastClose />
-          </Toast>
-        )
-      })}
+    <>
+      {toasts.map(({ id, title, description, action, ...props }) => (
+        <Toast key={id} {...props}>
+          <div className="grid gap-1">
+            {title && <ToastTitle>{title}</ToastTitle>}
+            {description && <ToastDescription>{description}</ToastDescription>}
+          </div>
+          {action}
+          <ToastClose onClick={() => dismiss(id)} />
+        </Toast>
+      ))}
       <ToastViewport />
-    </ToastProvider>
+    </>
   )
 }
+
+export default Toaster
